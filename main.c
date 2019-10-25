a84bbc2e78d551385788f624cd1dc00f895d1916
Josh Boudreau <46575851+joshuaboud@users.noreply.github.com>
2019-10-25 16:18:17 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define NUM_SHAPES 4
+#define ADC_RESOLUTION 1024UL
+#define TIMER1_RESOLUTION 65536UL
 
 enum {SINE, RAMP, TRI, SQU};
-#define NUM_SHAPES 4
+enum {UP, DOWN};
 
-unsigned char sine[128] = {
+uint8_t sine[128] = {
   0, 2, 3, 5, 6, 8, 9, 11,
   12, 14, 16, 17, 19, 20, 22, 23,
   25, 26, 28, 29, 31, 32, 34, 35,
@@ -22,58 +27,92 @@ unsigned char sine[128] = {
   126, 127, 127, 127, 127, 127, 127, 127
 };
 
-enum {UP, DOWN};
+volatile uint8_t quadrant = 0;
+volatile uint8_t sine_itr = 0;
+volatile uint8_t direction = UP;
+volatile uint8_t shape = SINE;
+volatile uint32_t freq_setting;
 
-int main(){
-  unsigned char OCR0A = 0;
-  
-  unsigned char i = 0;
-  unsigned char shape = SINE;
-  unsigned char quadrant = 0;
-  unsigned char direction = UP;
-  while(1){
-    switch(shape){
-    case SINE:
-      switch(quadrant){
-      case 0:
-        OCR0A = 128 + sine[i++];
-        if(i >= 127) quadrant++;
-        break;
-      case 1:
-        OCR0A = 128 + sine[i--];
-        if(i == 0) quadrant++;
-        break;
-      case 2:
-        OCR0A = 128 - sine[i++];
-        if(i >= 127) quadrant++;
-        break;
-      case 3:
-        OCR0A = 128 - sine[i--];
-        if(i == 0) quadrant = 0;
-        break;
-      }
+ISR (TIMER1_OVF_vect){
+  switch(shape){
+  case SINE:
+    switch(quadrant){
+    case 0:
+      PORTD = 128 + sine[sine_itr++];
+      if(sine_itr >= 127) quadrant++;
       break;
-    case RAMP:
-      OCR0A = (++OCR0A)%256;
+    case 1:
+      PORTD = 128 + sine[sine_itr--];
+      if(sine_itr == 0) quadrant++;
       break;
-    case TRI:
-      switch(direction){
-      case UP:
-        if(++OCR0A == 255) direction = DOWN;
-        break;
-      case DOWN:
-        if(--OCR0A == 0) direction = UP;
-        break;
-      }
+    case 2:
+      PORTD = 128 - sine[sine_itr++];
+      if(sine_itr >= 127) quadrant++;
       break;
-    case SQU:
-      OCR0A = (((++i)%128) > 63)? 255 : 0;
+    case 3:
+      PORTD = 128 - sine[sine_itr--];
+      if(sine_itr == 0) quadrant = 0;
       break;
     }
-    
-    // wait for 1/512th of period
-    
-    printf("%d\n",OCR0A);
+    break;
+  case RAMP:
+    PORTD++;
+    break;
+  case TRI:
+    switch(direction){
+    case UP:
+      if(++PORTD == 255) direction = DOWN;
+      break;
+    case DOWN:
+      if(--PORTD == 0) direction = UP;
+      break;
+    }
+    break;
+  case SQU:
+    PORTD = (((++sine_itr)%128) > 63)? 255 : 0;
+    break;
+  }
+  // calculate new TCNT1
+  TCNT1 = freq_setting * (TIMER1_RESOLUTION - 2UL) / ADC_RESOLUTION;
+}
+
+void initADC(){
+  // Select Vref=AVcc
+  ADMUX |= (1<<REFS0);
+  //set prescaler to 128 and enable ADC
+  ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
+}
+
+uint16_t readADC(uint8_t ADCchannel){
+  //select ADC channel with safety mask
+  ADMUX = (ADMUX & 0xF0) | (ADCchannel & 0x0F);
+  //single conversion mode
+  ADCSRA |= (1<<ADSC);
+  // wait until ADC conversion is complete
+  while( ADCSRA & (1<<ADSC) );
+  return ADC;
+}
+
+void initTimer(){
+  // init timer1
+  TCCR1A = 0x00;
+	TCCR1B = (1 << CS10);  // Timer mode with no prescler
+	TIMSK1 = (1 << TOIE1);   // Enable timer1 overflow interrupt(TOIE1)
+  TCNT1 = 0x0;
+}
+
+int main(){
+  PORTD = 0x0;
+  
+  initADC();
+  
+  initTimer();
+  
+  sei(); // Enable global interrupts by setting global interrupt enable bit in SREG
+  
+  while(1){
+    // poll ADC input for frequency setting
+    freq_setting = (uint32_t)readADC(0);
   }
   return 0;
 }
